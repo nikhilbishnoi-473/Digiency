f44144a09adb7ae0c0f8750d254f0bcd225756a0
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2023-12-27 16:13:49 -1200

    slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,45 @@
 $('.responsive').slick({
     dots: false,
-    infinite: true,
-    speed: 300,
-    autoplay: false,
+    infinite: false,
     nextArrow: ".next_btn",
     prevArrow: ".prev_btn",
+    speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
-
     responsive: [
-        {
-            breakpoint: 2000,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false,
-            }
-        },
-        {
-            breakpoint: 1440,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-            }
-        },
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-            }
-        },
-        {
-            breakpoint: 1040,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                autoplay:true,
-            }
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
         }
-
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        
+        }
+      }
     ]
-});  
+  });
+  
+ 
