6ab15656f59858449db443e42a1e01b00c4f7a09
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-01-05 12:31:40 -1200

    change slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,56 @@
 $('.responsive').slick({
     dots: false,
-    infinite: false,
+    infinite: true,
+    speed: 300,
+    autoplay: false,
     nextArrow: ".next_btn",
     prevArrow: ".prev_btn",
-    speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
+
     responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
+        {
+            breakpoint: 2000,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 1440,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 1040,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay:true,
+            }
         }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        
-        }
-      }
+
     ]
-  });
-  
- 
+});  
